29f0f641dd80fd22249f59f7daaf767946af506a
Magda Chudzik <kontakt@magdachudzik.pl>
2023-08-31 22:24:47 +0200

    fix(translations): Fix missing translations

--- src/i18n/pl/index.ts
+++ src/i18n/pl/index.ts
@@ -1,6 +1,7 @@
 import * as AboutMePage from './AboutMePage.json';
 import * as BwLandscapePage from './BwLandscapePage.json';
 import * as CityPage from './CityPage.json';
+import * as ColorLandscapePage from './ColorLandscapePage.json';
 import * as ContactPage from './ContactPage.json';
 import * as Footer from './Footer.json';
 import * as HomePage from './HomePage.json';
@@ -10,4 +11,17 @@ import * as PageNotFound from './PageNotFound.json';
 import * as PrintsPage from './PrintsPage.json';
 import * as Spinner from './Spinner.json';
 
-export { AboutMePage, BwLandscapePage, CityPage, ContactPage, Footer, HomePage, LandscapePage, Menu, PageNotFound, PrintsPage, Spinner };
+export {
+	AboutMePage,
+	BwLandscapePage,
+	CityPage,
+	ColorLandscapePage,
+	ContactPage,
+	Footer,
+	HomePage,
+	LandscapePage,
+	Menu,
+	PageNotFound,
+	PrintsPage,
+	Spinner
+};
