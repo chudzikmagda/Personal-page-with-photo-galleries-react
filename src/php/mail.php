deb28839d82a24131db7df14d23d6de4db5933b7
Magda Chudzik <kontakt@magdachudzik.pl>
2023-11-08 22:34:15 +0100

    Fix(ContactPage): Fix php script and move it into src folder

---
+++ src/php/mail.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['name']) && isset($_POST['message']) && isset($_POST['email'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $message = htmlspecialchars($_POST['message']);
+    $email = $_POST['email'];
+    $recipient = "hi@magdachudzik.pl";
+    $subject = "Wiadomość z portfolio fotograficznego";
+    $formcontent = "From: $name\n\nEmail: $email\n\nMessage: $message\n\n";
+    
+    $mailheader = "From: $email\r\n";
+
+    if (mail($recipient, $subject, $formcontent, $mailheader)) {
+        echo "Email sent successfully";
+    } else {
+        echo "Email could not be sent";
+    }
+} else {
+    $error_message = "POST data is missing";
+    die($error_message);
+}
+?>
