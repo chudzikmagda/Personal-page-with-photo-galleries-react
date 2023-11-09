3cec3ed396e48ac3eff1d1f581bc681ae20dda66
Magda Chudzik <kontakt@magdachudzik.pl>
2023-11-09 21:16:04 +0100

    feat: Add Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import reportWebVitals from './reportWebVitals';
 import './i18n';
 import './index.scss';
 import App from './App';
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = 'G-11Z8MHVWS1';
+
+ReactGA.initialize(TRACKING_ID);
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
