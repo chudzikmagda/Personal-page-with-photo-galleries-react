32df60dda585dd4bfa7a14070fc0feaefae1dcf0
Magda Chudzik <kontakt@magdachudzik.pl>
2023-11-09 21:25:48 +0100

    feat: Add Hotjar tracking

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import './i18n';
 import './index.scss';
 import App from './App';
 import ReactGA from 'react-ga4';
+import { hotjar } from 'react-hotjar';
 
 const TRACKING_ID = 'G-11Z8MHVWS1';
+const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 ReactGA.initialize(TRACKING_ID);
 
-const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+hotjar.initialize(1141369, 6);
 
 root.render(
 	<React.StrictMode>
